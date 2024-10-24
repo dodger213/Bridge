66131a52f93353b4a51d32e4a70db54fece87d0d
BTC415 <alenschmitt415@gmail.com>
2024-10-25 00:57:36 +0100

    [add] added internal functions

--- contracts/src/ProxyOFTWithFee.sol
+++ contracts/src/ProxyOFTWithFee.sol
@@ -32,15 +32,59 @@ contract ProxyOFTWithFee is BaseOFTWithFee {
         );
         ld2sdRate = 10 ** (decimals - _sharedDecimals);
     }
-    
+
     /************************************************************************
-    * public functions
-    ************************************************************************/
+     * public functions
+     ************************************************************************/
     function circulatingSupply() public view virtual override returns (uint) {
         return innerToken.totalSupply() - outboundAmount;
     }
 
     function token() public view virtual override returns (address) {
         return address(innerToken);
+    }
+
+       /************************************************************************
+    * internal functions
+    ************************************************************************/
+    function _debitFrom(address _from, uint16, bytes32, uint _amount) internal virtual override returns (uint) {
+        require(_from == _msgSender(), "ProxyOFTWithFee: owner is not send caller");
+
+        _amount = _transferFrom(_from, address(this), _amount);
+
+        // _amount still may have dust if the token has transfer fee, then give the dust back to the sender
+        (uint amount, uint dust) = _removeDust(_amount);
+        if (dust > 0) innerToken.safeTransfer(_from, dust);
+
+        // check total outbound amount
+        outboundAmount += amount;
+        uint cap = _sd2ld(type(uint64).max);
+        require(cap >= outboundAmount, "ProxyOFTWithFee: outboundAmount overflow");
+
+        return amount;
+    }
+
+    function _creditTo(uint16, address _toAddress, uint _amount) internal virtual override returns (uint) {
+        outboundAmount -= _amount;
+
+        // tokens are already in this contract, so no need to transfer
+        if (_toAddress == address(this)) {
+            return _amount;
+        }
+
+        return _transferFrom(address(this), _toAddress, _amount);
+    }
+
+    function _transferFrom(address _from, address _to, uint _amount) internal virtual override returns (uint) {
+        uint before = innerToken.balanceOf(_to);
+        if (_from == address(this)) {
+            innerToken.safeTransfer(_to, _amount);
+        } else {
+            innerToken.safeTransferFrom(_from, _to, _amount);
+        }
+        return innerToken.balanceOf(_to) - before;
+    }
+    function _ld2sdRate() internal view virtual override returns (uint) {
+        return ld2sdRate;
     }
 }
