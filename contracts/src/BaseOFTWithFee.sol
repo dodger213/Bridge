c9aa76d34245e87e1df94cca50e84672d146f6e9
BTC415 <alenschmitt415@gmail.com>
2024-10-24 11:04:07 +0100

    [add] added constructor

--- contracts/src/BaseOFTWithFee.sol
+++ contracts/src/BaseOFTWithFee.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
 import "../OFTCoreV2.sol";
@@ -7,4 +6,9 @@ import "./IOFTWithFee.sol";
 import "./Fee.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
-abstract contract BaseOFTWithFee is OFTCoreV2, Fee, ERC165, IOFTWithFee {}
+abstract contract BaseOFTWithFee is OFTCoreV2, Fee, ERC165, IOFTWithFee {
+    constructor(
+        uint8 _sharedDecimals,
+        address _lzEndpoint
+    ) OFTCoreV2(_sharedDecimals, _lzEndpoint) {}
+}
