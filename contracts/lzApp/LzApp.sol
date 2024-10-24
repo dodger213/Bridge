fa07727eee361f1c7ce87d3a97e8f191166c9406
BTC415 <alenschmitt415@gmail.com>
2024-10-24 01:41:57 +0100

    [add] add view func in lzApp sol

--- contracts/lzApp/LzApp.sol
+++ contracts/lzApp/LzApp.sol
@@ -220,4 +220,10 @@ abstract contract LzApp is
     ) external onlyOwner {
         payloadSizeLimitLookup[_dstChainId] = _size;
     }
+
+    //--------------------------- VIEW FUNCTION ----------------------------------------
+    function isTrustedRemote(uint16 _srcChainId, bytes calldata _srcAddress) external view returns (bool) {
+        bytes memory trustedSource = trustedRemoteLookup[_srcChainId];
+        return keccak256(trustedSource) == keccak256(_srcAddress);
+    }
 }
