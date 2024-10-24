2caf76107edefa23607cdf0c641fd24b973d5bf8
BTC415 <alenschmitt415@gmail.com>
2024-10-24 11:00:07 +0100

    [create] created BaseOFTWithFee sol

---
+++ contracts/src/BaseOFTWithFee.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../OFTCoreV2.sol";
+import "./IOFTWithFee.sol";
+import "./Fee.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+abstract contract BaseOFTWithFee is OFTCoreV2, Fee, ERC165, IOFTWithFee {}
